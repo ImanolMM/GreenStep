bc1ff36f50d8e3892f5ac903a1291b932d3e2d68
Diego <dnido001@ikasle.ehu.eus>
2023-04-20 09:49:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import {useEffect, useState} from "react";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAp598ZuAB8v2rOsE2wl_7AkNmpAdFKyKk",
+  authDomain: "prueba-d528d.firebaseapp.com",
+  projectId: "prueba-d528d",
+  storageBucket: "prueba-d528d.appspot.com",
+  messagingSenderId: "715965267025",
+  appId: "1:715965267025:web:dc0c72a222d34e57f0cc70"
+})
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+/*
+firestore.collection('imanoltxitu').where('nombre', '==', 'imanol').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        console.log(doc.data());
+    })
+});
+firestore.collection('imanoltxitu').add({nombre : "imanol"}
+
+
+ */
+firestore.collection('imanoltxitu').add({nombre : "imanol"});
+// Deleting document with nombre imanol from the collection
+// firestore.collection('imanoltxitu').where('nombre', '==', 'imanol').get().then((snapshot) => {
+
+firestore.collection('imanoltxitu').where('nombre', '==', 'imanol').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        doc.ref.delete().catch((error) => {console.log("hola")});
+    })
+});
+
+// Updating document with nombre imanol from the collection
+firestore.collection('imanoltxitu').where('nombre', '==', 'imanol').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        doc.ref.update({nombre : "imanol updated"}).catch((error) => {console.log("hola2")});
+    });
+});
 
 function App() {
+  const [user] = useAuthState(auth);
+  const [datos] = useCollectionData(firestore.collection('imanoltxitu'));
+  const [imagen, setImagen] = useState('');
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+      fetch('https://dog.ceo/api/breeds/image/random').then((response) => response.json()).then
+      ((data) => {
+          setImagen(data.message);
+      });
+  }, [contador]);
+
+console.log(datos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header>
+        <h1>Chap</h1>
+
+        </header>
+        <section>
+            {user ? <ChatRoom /> : <SignIn />}
+            <SignOut />
+            <img src={imagen} alt="imagen" />
+            <button onClick={() => setContador(contador + 1)}>Contador</button>
+        </section>
     </div>
   );
+}
+
+function ChatRoom() {}
+function SignIn() {
+const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
 }
 
 export default App;
